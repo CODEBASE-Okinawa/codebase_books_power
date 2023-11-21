95ec555fb02bfac95ce96d6b9ddc44135e51d3ae
touyama <yusuke2710@icloud.com>
2023-11-21 21:13:42 +0900

    book image add

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -24,14 +24,22 @@ class Admin::BooksController < ApplicationController
   def search
     require 'net/http'
     require 'uri'
-
+  
     isbn10 = params[:isbn]
     uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     response = http.get("#{uri}?q=isbn:#{isbn10}")
     hash = JSON.parse(response.body)
-    @result_book = hash["items"][0]["volumeInfo"]
+  
+    # レスポンスが正常かつ"items"が存在する場合のみ@result_bookに代入する
+    if hash["items"].present? && hash["items"].is_a?(Array) && hash["items"].first.present?
+      @result_book = hash["items"][0]["volumeInfo"]
+    else
+      # それ以外の場合は@result_bookをnilに設定する
+      @result_book = nil
+    end
+  
     redirect_to action: 'new', result_book: @result_book
   end
 
