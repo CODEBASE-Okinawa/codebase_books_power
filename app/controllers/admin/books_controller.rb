20210518a71cad713571d75c1c024c030ae98c5d
touyama <yusuke2710@icloud.com>
2023-11-23 15:38:47 +0900

    new update

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -12,7 +12,12 @@ class Admin::BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.image.attach(params[:book][:image])
+
+    # 画像のURLがあればそれを利用
+    if params[:book][:image_url].present?
+      @book.image_url = params[:book][:image_url]
+    end
+
     if @book.save
       flash[:success] = "本を登録しました"
       redirect_to admin_books_path
@@ -21,27 +26,46 @@ class Admin::BooksController < ApplicationController
     end
   end
 
+
   def search
     require 'net/http'
     require 'uri'
   
     isbn10 = params[:isbn]
+    title = params[:title]
+    query = if isbn10.present?
+              "isbn:#{isbn10}"
+            elsif title.present?
+              title
+            else
+              ""
+            end
+  
     uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    response = http.get("#{uri}?q=isbn:#{isbn10}")
+    response = http.get("#{uri}?q=#{URI.encode_www_form_component(query)}")
     hash = JSON.parse(response.body)
   
-    # レスポンスが正常かつ"items"が存在する場合のみ@result_bookに代入する
     if hash["items"].present? && hash["items"].is_a?(Array) && hash["items"].first.present?
-      @result_book = hash["items"][0]["volumeInfo"]
-    else
-      # それ以外の場合は@result_bookをnilに設定する
-      @result_book = nil
-    end
+      @result_book = hash["items"].first["volumeInfo"]
   
-    redirect_to action: 'new', result_book: @result_book
+      if @book.present? && @result_book["imageLinks"] && @result_book["imageLinks"]["thumbnail"]
+        @book.image_url = @result_book["imageLinks"]["thumbnail"]
+        @book.save
+      end
+  
+      redirect_to action: 'new', result_book: @result_book
+    else
+      @result_book = nil
+      redirect_to action: 'new', result_book: @result_book
+    end
   end
+  
+  
+  
+  
+  
 
   private
 
