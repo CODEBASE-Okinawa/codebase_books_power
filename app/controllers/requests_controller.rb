c71f00addd68a4343aa5593319c296498c8b6dc1
HY <givhvyvuc@gmail.com>
2023-11-25 05:24:16 +0900

    request登録きのう

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,20 +1,39 @@
 class RequestsController < ApplicationController
-
     def new
     end
 
     def search
         require 'net/http'
         require 'uri'
-
-        isbn10 = params[:isbn]
         uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
+        text = params[:search]
+
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        response = http.get("#{uri}?q=isbn:#{isbn10}")
-        hash = JSON.parse(response.body)
-        @result_book = hash["items"][0]["volumeInfo"]
-        redirect_to '/books/search'
+        http.use_ssl = true        
+        response = http.get("#{uri}?q=search:#{text}")
+
+        @google_books = JSON.parse(response.body)
     end
+
+    def create
+
+        @bookRequest = Request.new 
+        #モデルに書いたsave_with_authorメソッドを実行する
+
+        if @bookRequest.save_with_request(params[:title], params[:systemid], params[:book][:authors])
+          redirect_to books_path, success: t('.success')
+        else
+          flash.now[:danger] = t('.fail')
+        end
+      end
+    
+      private  
+      
+      #うまく使えなかった
+      def requests_params
+        params.require(:book).permit(:title, :systemid, authors: [])
+      end
+    
+    
 
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,14 @@
 class Request < ApplicationRecord
+  def save_with_request(title, systemid, authors)
+    ActiveRecord::Base.transaction do
+      puts title
+      self.title = title
+      self.author = authors.first.strip
+      self.isbn = systemid
+      self.save!
+    end
+    true
+  rescue StandardError
+    false
+  end
 end
