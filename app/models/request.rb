1bb182d393524cc520d1dfdbd2247eca9fe56a18
koikeasuka <akoike@big-gate.co.jp>
2023-11-26 12:16:34 +0900

    Add: user_id to create method

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,11 +1,12 @@
 class Request < ApplicationRecord
   belongs_to :user
-  def save_with_request(title, systemid, authors)
+  def save_with_request(title, systemid, authors, user_id)
     ActiveRecord::Base.transaction do
       puts title
       self.title = title
       self.author = authors.first.strip
       self.isbn = systemid
+      self.user_id = user_id
       self.save!
     end
     true
