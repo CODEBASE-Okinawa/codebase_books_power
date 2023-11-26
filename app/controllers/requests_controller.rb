6f9b0cbaac13962964659cf2371e2369a6f4fcc1
HY <givhvyvuc@gmail.com>
2023-11-26 13:51:32 +0900

    requestする本のステータス

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,36 +5,41 @@ class RequestsController < ApplicationController
     end
 
     def search
-        require 'net/http'
-        require 'uri'
-        uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
-        text = params[:search]
+      require 'net/http'
+      require 'uri'
+      uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
+      text = params[:search]
 
+      # 検索窓がブランクの時
+      if text.present?
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true        
         response = http.get("#{uri}?q=search:#{text}")
-
         @google_books = JSON.parse(response.body)
+      else
+        @google_books = nil
+      end
+
     end
 
     def create
-        @bookRequest = Request.new 
-        #モデルに書いたsave_with_authorメソッドを実行する
+      @bookRequest = Request.new 
+      #モデルに書いたsave_with_authorメソッドを実行する
 
-        if @bookRequest.save_with_request(params[:title], params[:systemid], params[:book][:authors])
-          notifier.ping("本のリクエストがありました。ISNB:#{params[:systemid]}")
-          redirect_to books_path, success: t('.success')
-        else
-          flash.now[:danger] = t('.fail')
-        end
+      if @bookRequest.save_with_request(params[:title], params[:systemid], params[:book][:authors])
+        notifier.ping("本のリクエストがありました。ISNB:#{params[:systemid]}")
+        redirect_to books_path, success: t('.success')
+      else
+        flash.now[:danger] = t('.fail')
       end
+    end
     
       private  
       
-      #うまく使えなかった
-      def requests_params
-        params.require(:book).permit(:title, :systemid, authors: [])
-      end
+      # #うまく使えなかった
+      # def requests_params
+      #   params.require(:book).permit(:title, :systemid, authors: [])
+      # end
     
   def notifier
     Slack::Notifier.new(WEBHOOK_URL, username: 'Codebase Book')
